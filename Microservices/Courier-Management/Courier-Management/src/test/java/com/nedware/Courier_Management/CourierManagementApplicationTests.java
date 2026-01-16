8e12676975a756fac968c4e46a5834af3fc7cc7b
Matheus Ned <matheusned15@gmail.com>
2026-01-16 17:08:44 -0300

    Desing Tatico e Domain Model

---
+++ Microservices/Courier-Management/Courier-Management/src/test/java/com/nedware/Courier_Management/CourierManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nedware.Courier_Management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourierManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
